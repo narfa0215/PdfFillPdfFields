a70b6ee9208e6f7fc70f128ad2f4565189a274ba
narfa0215 <124590168+narfa0215@users.noreply.github.com>
2026-04-30 17:55:00 +0800

    完成文本域填充开发

---
+++ PdfFillPdfFields/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using iText.Forms;
+using iText.Forms.Fields;
+using iText.Forms.Xfdf;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+
+namespace PdfFillPdfFields
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("PdfFillPdfFields.Template.Table.pdf"))
+            using (var pdfDoc = new PdfDocument(new PdfReader(resourceStream), new PdfWriter("output.pdf")))
+            {
+                var form = PdfAcroForm.GetAcroForm(pdfDoc, true);
+
+                var page = pdfDoc.GetPage(1);
+
+                float x = 90.504f;
+                float y = 698.98f;
+                float width = 219.02f;
+                float height = 70.464f;
+
+                var textField = new TextFormFieldBuilder(pdfDoc, "CellText")
+                    .SetPage(1)
+                    .SetWidgetRectangle(new Rectangle(x, y, width, height))
+                    .CreateText();
+                
+                form.AddField(textField);
+
+                pdfDoc.Close();
+            }
+        }
+    }
+}
